bc32273f567a109a8a90063f09ed3afdd8dcaec1
Job2x <eslamahmed007x>
2026-04-08 14:24:53 +0200

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app.controller.js

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,11 +1,20 @@
 import connectDb from "./DB/connectDB.js";
 import globalErrorHandler from "./Utlis/errorHandler.utlis.js"
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./Modules/auth/auth.controller.js"
 import productRouter from "./Modules/Product/product.controller.js"
 import categoryRouter from "./Modules/Category/categort.controller.js"
 
 const bootStrap = async (app, express) => {
+    // CORS - Allow all origins
+    app.use(cors({
+        origin: '*',
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }))
+
     app.use(express.json())
     app.use(cookieParser());
 
